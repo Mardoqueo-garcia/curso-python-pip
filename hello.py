a3b1a950d04980d7c2e3c8fc4e8a7e46299f9646
Otoniel Garcia <mardoqueogarcia112016@gmail.com>
2023-10-08 18:55:03 -0600

    Sin novedad alguna

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 print('Creando cosas avanzadas desde la terminal y git')
 print("Estamos editando dentro del editor de texto de vim")
 #Con este podemos editar nuestro archivo  sin salir de la terminal
-#Para editar presiona i, para salir de editar presiona esc, para salir del editor y guardar escribe :wq
+#Para editar presiona i, para salir de editar presiona esc, para salir del editor y guardar tus cambios  escribe :wq
 print("Saludos mis amigos")
+print("Seria bueno aprender un poco mas de vim, asi evitamos salir de la terminal y ahorramos tiempo")
+for ele in range(1,14):
+    if ele <8:
+        print("Este viene del for ==> ",ele)
